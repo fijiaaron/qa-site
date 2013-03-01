772442889e4a9ee5a6ab8e7d41eb950eb6f147e1
qasite <qasite@li21-194.members.linode.com>
2013-03-01 20:19:44 +0000

    Created repo with HTML version of "What is a QA Site"

---
+++ site/public/index.php
@@ -0,0 +1,149 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>QA Site</title>
+	<style>
+		body {
+			margin: 0;
+			padding: 2em;
+			font-family: Arial;
+			width; 900px;
+			max-width: 900px;
+			min-width: 900px;
+		}
+		h1, h2, h3 { 
+			color: #6b7;
+			font-weight: bold;
+			font-style: italic;
+
+			margin: 0;
+			padding: 0;
+		}
+
+		h1 {
+			font-size: 2em;
+			margin: 1em 0;
+			border-bottom: 1px dotted #333;
+		}
+		h2 {
+			font-size: 1.5em;
+			padding: 1em 0 0.25em; 
+		}
+		h3 {
+			font-size: 1.2em;
+			padding: 0 0 0 10em;
+		}
+
+		p {
+			font-size: 1.1em;
+			color: #000;
+			max-width: 850px;
+		}
+
+		span.qasite {
+			color: #6b6;
+			font-style: italic;
+			font-weight: bold;
+		}
+
+		span.highlight {
+			color: #264;
+		}
+
+		ul {
+			list-style-type: none;
+			
+		}
+
+		ul.checkmark li:before {
+			content:"\2713\0020";
+			padding: 0.5em;
+		}
+
+
+
+	ul { 
+		width: 40em;
+		margin: 1em 3em 1em 7em;
+		overflow: hidden;
+		/*
+			border-top:1px solid #ccc;
+		*/
+	}
+
+	li {
+		/*
+		line-height:1.5em;
+		border-bottom:1px solid #ccc;
+		*/
+		float:left;
+		display:inline;
+	}
+
+	ul.double li {
+		width: 50%;
+	}
+		  
+	</style>
+</head>
+<body>
+	<h1> What is a QA Site? </h1>
+	<h2> QA Site is a testing <span class="highlight">dashboard</span> </h2>
+	<h3> that enables you to see at a glance your testing progress and project status</h3>
+
+	<p> 
+	You <span class="qasite">QA Site</span> dashboard is a home page that summarizes your QA efforts and provides links to your testing, development, and project management tools.
+	</p>
+
+	<p>
+	See project status, test execution results, defects, builds, and other notifications all in one place.
+	</p>
+
+	<h2>QA Site is a collection of software <span class="highlight">tools</span></h2>
+	<h3>that you use to improve quality and release with greater confidence.</h3>
+
+	<p>
+	Choose the tools you want to use, commercial and open source, working together. No vendor lock-in. You don't have to change your existing infrastructure or processes. 
+	</p>
+
+	<p>Your <span class="qasite">QA Site</span> can include:
+
+	<ul class="checkmark double">
+		<li>Test Cases</li>
+		<li>Automation</li>
+		<li>Continuous Integration</li>
+		<li>Code Analysis</li>
+	
+		<li>Version Control</li>
+		<li>Defect Tracking</li>
+		<li>Documentation</li>
+		<li>Project Management</li>
+	</ul>
+
+	<h2>QA Site is a hosted <span class="highlight">platform</span></h2>
+	<h3>that you don't have to setup or maintain.</h3>
+
+	<p>
+	Deploy your QA Site on your favorite cloud platform or within your own network. No setup or infrastructure costs. No in-house expertise or maintenance required.
+	</p>
+
+	<p>
+	We will set it up and work to integrate your existing tools to build a custom dashboard & reports that fit your needs. 
+	</p>
+
+	<p>
+	Training and support are available, and the system is maintained for you.
+	</p>
+
+
+	<div style="text-align: center; margin: 4em">
+		<h2>Request your QA site today</h2>
+		<div style="font: 1.8em arial bold; margin: 0.6em;">425-242-4304</div>
+		<img src="http://www.one-shore.com/images/one-shore-logo.jpg"/>
+	</div>
+</body>
+</html>
+
